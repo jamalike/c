5e0d5d3d0cc217e85174d6b013c32995272a00aa
Paulo Silva <paulo.silva@viannasempre.com.br>
2023-09-23 20:30:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write(Multiplicar(7,3));
+    }
+
+    public static int Multiplicar(int m, int n)
+    {
+        int resultado = 0;
+        for (int i=0; i< n; i++)
+        {
+            resultado += m;
+        }
+        return resultado;
+    }
+}
